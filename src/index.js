2c6a279dad843f8ccbc1f057215a9d724c6a1a82
maimuna omar <maymunaabdi07@gmail.com>
2023-05-24 14:41:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentComponent from './components/Parentcomponent';
 
 ReactDOM.render(
-  <Form />,
+  <ParentComponent /> ,
   document.getElementById('root')
 );
